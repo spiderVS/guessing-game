dfe33c8964978419735ac7119a5468cc75401d8c
spiderVS <fuzzy003@gmail.com>
2021-09-03 23:15:14 +0300

    feat: fix guess method, add other methods --> solved task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -7,15 +7,17 @@ class GuessingGame {
     }
 
     guess() {
-      return (this.min + Math.floor((this.max - this.min)/2));
+      return (this.min + Math.ceil((this.max - this.min)/2));
     }
 
     lower() {
-      
+      this.setRange(this.min, this.guess());
+      this.guess();
     }
 
     greater() {
-
+      this.setRange(this.guess(), this.max);
+      this.guess();
     }
 }
 
