d15e7cd9331802c16dedce6e3991fe15a3860dfd
spiderVS <fuzzy003@gmail.com>
2021-09-03 23:03:36 +0300

    init: start task, add guess method

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,15 +2,16 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      return (this.min + Math.floor((this.max - this.min)/2));
     }
 
     lower() {
-
+      
     }
 
     greater() {
